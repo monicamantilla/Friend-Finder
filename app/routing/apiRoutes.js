ca11163bb42a4e83e34c500d7a23ebad4b9ed5c1
Monica Mantilla <monicamantilla@MacBook-Pro-2.local>
2019-06-11 20:43:07 -0400

    working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,24 +1,54 @@
 //A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 //A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
-var friends = require("../data/friends.js");
+// var friends = require("../data/friends.js");
+
+//Save the apps data here as an array of objects
+
+var friends = [
+    {
+    name : "Monica",
+    photo: " ",
+    scores: [2, 3, 4, 5, 6, 7, 3 ,4, 6, 1]
+},
+{
+    name : "Mike",
+    photo: " ",
+    scores: [2, 3, 4, 5, 6, 7, 3 ,4, 6, 1]
+},
+{
+    name : "Mark",
+    photo: " ",
+    scores: [2, 3, 4, 5, 6, 7, 3 ,4, 6, 1]
+},
+{
+    name : "Stephanie",
+    photo: " ",
+    scores: [2, 3, 4, 5, 6, 7, 3 ,4, 6, 1]
+}
+];
+
+module.export = friends; 
 
 module.exports = function (app){
     app.get("/api/friends", function (req, res){
         res.json(friends);
     })
 
-    app.post("/api/friends", function(req, res){
-        var totalDifference = 0;
+    app.post("/api/new", function(req, res){
+        var scoreDiff = 0;
+
+
         var bestMatch = {
             name: "",
             photo: "",
             friendDifference: 1000
         };
+        console.log(req.body);
 
         var userData = req.body;
         var userName = userData.name;
-        var userScores = userData.score;
+        var userScores = userData.scores;
 
         var b  = userScores.map(function(item){
             return parseInt(item, 10);
@@ -26,7 +56,7 @@ module.exports = function (app){
         userData = {
             name: req.body.name,
             photo: req.body.photo,
-            score: b
+            scores: b
         };
         console.log("Name: " + userName);
         console.log("Scores: " + userScores );
@@ -35,25 +65,28 @@ module.exports = function (app){
          console.log("Sum of user scores" +  sum);
          console.log("Best match friend diff" + bestMatch.friendDifference);
 
+         console.log(friends);
          for(var i = 0; i < friends.length; i++){
              console.log(friends[i].name);
-             totalDifference = 0;
-             console.log("Total Diff" + totalDifference);
+             scoreDiff = 0;
+             console.log("Total Diff" + scoreDiff);
              console.log("Best match friend diff" + bestMatch.friendDifference);
 
              var bfriendScore = friends[i].scores.reduce((a,b) => a + b, 0 );
              console.log("Total Friend Score" + bfriendScore);
-             totalDifference += Math.abs(sum - bfriendScore);
-             console.log("----->" + totalDifference);
+             scoreDiff += Math.abs(sum - bfriendScore);
+             console.log("----->" + scoreDiff);
 
-             if(totalDifference <= bestMatch.friendDifference){
+             if(scoreDiff <= bestMatch.friendDifference){
                  bestMatch.name = friends[i].name;
                  bestMatch.photo = friends[i].photo;
-                 bestMatch.friendDifference = totalDifference;
+                 bestMatch.friendDifference = scoreDiff;
              }
-             console.log(totalDifference + "Total Difference")
+             console.log(scoreDiff + "Total Difference")
          }
             console.log(bestMatch);
+
+            console.log(friends);
             friends.push(userData);
             console.log("New User Added");
             console.log(userData);
